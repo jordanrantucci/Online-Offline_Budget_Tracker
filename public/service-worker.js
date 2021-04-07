58743f0bc3550622336233847804718b7b9f6709
Jordan Rantucci <jordanrantucci@gmail.com>
2021-04-06 22:48:03 -0400

    fixing service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,21 +6,23 @@ const FILES_TO_CACHE = [
     '/dist/bundle.js',
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
-    '/icons'
+    '/icons',
+    '/manifest.webmanifest',
+    '/index.js'
 ];
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const CACHE_NAME = 'static-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v1';
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
-            .open(PRECACHE)
+            .open(CACHE_NAME)
             .then((cache) => cache.addAll(FILES_TO_CACHE))
             .then(self.skipWaiting())
     );
 });
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
-    const currentCaches = [PRECACHE, RUNTIME];
+    const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
     event.waitUntil(
         caches
             .keys()
@@ -37,21 +39,31 @@ self.addEventListener('activate', (event) => {
             .then(() => self.clients.claim())
     );
 });
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith(self.location.origin)) {
+
+self.addEventListener('fetch', function(event){
+    if (event.request.url.includes("/api/")){
         event.respondWith(
-            caches.match(event.request).then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-                return caches.open(RUNTIME).then((cache) => {
-                    return fetch(event.request).then((response) => {
-                        return cache.put(event.request, response.clone()).then(() => {
-                            return response;
-                        });
-                    });
-                });
-            })
-        );
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200){
+                        cache.put(event.request.url, response.clone())
+                    }
+                    return response
+                })
+                .catch(err => {
+                    return cache.match(event.request)
+                })
+            }).catch(err => console.log(err))
+        )
+        return
     }
-});
+
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request)
+            })
+        })
+    )
+    })
